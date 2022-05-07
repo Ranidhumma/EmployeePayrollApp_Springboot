825f4db64ad96662f850ffb0cc7cfabfa61808b1
Ranidhumma <ranidhumma8@gmail.com>
2022-05-08 01:17:04 +0530

    Created a Rest Controller for various different HTTP Methods

--- src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,5 +1,4 @@
 package com.example.employeepayrollapp;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,17 @@
+package com.example.employeepayrollapp.dto;
+
+public class EmployeePayrollDTO {
+public String  name;
+public Long Salary;
+
+    public EmployeePayrollDTO(String name, Long salary) {
+        this.name = name;
+        this.Salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "name="+name+":salary="+Salary;
+
+    }
+}
