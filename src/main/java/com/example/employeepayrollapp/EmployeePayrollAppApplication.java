89f735c84190be81d67c8861896f34cd89946063
Ranidhumma <ranidhumma8@gmail.com>
2022-05-09 23:16:09 +0530

    Added Regex pattern for for Name field

--- src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 @Slf4j
 public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Payroll Application");
+		System.out.println("Welcome to Employee Payroll Application ");
 		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		log.info("Employee Payroll App Started in {} Environment",
 				context.getEnvironment().getProperty("environment"));
